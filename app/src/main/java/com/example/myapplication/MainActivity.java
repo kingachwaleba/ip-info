9d09da09da6373d5c071290aa876885c79d9bf50
kingachwaleba <kingach6@gmail.com>
2021-11-16 11:41:44 +0100

    Edit MainActivity class

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPress(view);
+            }
+        });
+    }
+
+    public void buttonPress(View view) {
+        getIPInfo();
+    }
+
+    private String getIP() {
+        EditText e1 = (EditText) findViewById(R.id.ip1);
+        EditText e2 = (EditText) findViewById(R.id.ip2);
+        EditText e3 = (EditText) findViewById(R.id.ip3);
+        EditText e4 = (EditText) findViewById(R.id.ip4);
+
+        String ip = e1.getText().toString() + "." + e2.getText().toString() + "." +
+                e3.getText().toString() + "." + e4.getText().toString();
+
+        return ip;
+    }
+
+    private void printInfo(IPInfo info) {
+        TextView textView = (TextView) findViewById(R.id.textView4);
+
+        String s;
+
+        if(info == null) s = "Faild";
+        else {
+            s = "ip: " + info.getIp() + "\n" +
+                    "hostname: " + info.getHostname() + "\n" +
+                    "city: " + info.getCity();
+        }
+        textView.setText(s);
+    }
+
+    private void getIPInfo(){
+        ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class, getIP());
+
+        Call<IPInfo> call = apiInterface.getIPInfo();
+
+        call.enqueue(new Callback<IPInfo>() {
+            @Override
+            public void onResponse(Call<IPInfo> call, Response<IPInfo> response) {
+                printInfo(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<IPInfo> call, Throwable t) {
+                printInfo(null);
+            }
+        });
     }
 }
